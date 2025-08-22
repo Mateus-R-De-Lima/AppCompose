c59f2c6ae65d8d1417f895a4ece620e13f537740
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-08-22 10:36:09 -0300

    fix: reorganização do projeto para facilitar o aprendizado
    Reestruturada a organização dos arquivos e pastas com foco em tornar o projeto mais didático e compreensível.

--- app/src/main/java/com/example/appcompose/MainActivity.kt
+++ app/src/main/java/com/example/appcompose/MainActivity.kt
@@ -4,23 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.appcompose.ui.theme.AppComposeTheme
-import com.example.appcompose.ui.theme.Typography
+import com.example.appcompose.ui.theme.component.HelloContent
 
 // MainActivity é a "porta de entrada" do app no Android
 // Ela herda de ComponentActivity, que já é preparada para usar Jetpack Compose
@@ -50,36 +39,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
-// @Composable marca funções que podem "desenhar" interface usando Compose
-// Pense como se fosse um "componente de tela" reutilizável
-@Composable
-fun HelloContent(modifier: Modifier = Modifier) {
-    // Column = layout em coluna (empilha elementos verticalmente)
-    // Adicionamos um padding geral de 16dp (espaço nas bordas)
-    Column(modifier = modifier.padding(16.dp)) {
-        // Criamos uma variável "name" que será observada pelo Compose
-        // remember -> salva o estado na memória enquanto a tela existir
-        // mutableStateOf("") -> cria um estado que pode mudar e que a tela vai "reagir"
-        var name by remember { mutableStateOf("") }
-        // Condição: se o usuário já digitou algo (não vazio)
-        if (name.isNotBlank()) {
-            // Mostra um texto de saudação, pegando a string do arquivo strings.xml
-            Text(
-                text = stringResource(R.string.hello_name, name), // "Olá, Mateus!"
-                modifier = Modifier.padding(bottom = 8.dp), // Espaço embaixo
-                style = Typography.bodyMedium // Estilo do texto
-            )
-        }
-        // Campo de texto com borda (Material Design)
-        OutlinedTextField(
-            value = name, // O que vai aparecer dentro do campo
-            onValueChange = { updatedName -> // Toda vez que o usuário digitar algo, atualizamos "name"
-            name = updatedName // Isso faz o Compose "recompor" a tela, exibindo a atualização
-
-        }, label = {
-                // Rótulo que aparece dentro do campo
-            Text(stringResource(R.string.name)) // "Digite seu nome"
-        })
-    }
-}
